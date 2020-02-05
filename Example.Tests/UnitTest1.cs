21832c254e4674a8b601bc1f9f333fede5a53725
Yanek <loytosh@gmail.com>
2020-02-05 22:30:57 +0200

    second commit

--- Example.Tests/UnitTest1.cs
+++ Example.Tests/UnitTest1.cs
@@ -8,6 +8,19 @@ namespace Example.Tests
         [TestMethod]
         public void TestMethod1()
         {
+			Console.WriteLine("¬вед≥ть перше число");
+            string a2 = Console.ReadLine();
+            int a = Convert.ToInt32(a2);
+
+            Console.WriteLine("¬вед≥ть друге число");
+            string b2 = Console.ReadLine();
+            int b = Convert.ToInt32(b2);
+
+            int c = a + b;
+
+            Console.WriteLine(a + " + " + b + " = " + c);
+
+            Console.ReadLine();
         }
     }
 }
