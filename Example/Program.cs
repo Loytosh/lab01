41faac9275a2cd68eba731b9b4e796e52f251897
Yanek <loytosh@gmail.com>
2020-02-05 22:41:08 +0200

    three commit

--- Example/Program.cs
+++ Example/Program.cs
@@ -7,19 +7,6 @@ namespace Example
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-			Console.WriteLine("Введіть перше число");
-            string a2 = Console.ReadLine();
-            int a = Convert.ToInt32(a2);
-
-            Console.WriteLine("Введіть друге число");
-            string b2 = Console.ReadLine();
-            int b = Convert.ToInt32(b2);
-
-            int c = a + b;
-
-            Console.WriteLine(a + " + " + b + " = " + c);
-
-            Console.ReadLine();
         }
     }
 }
